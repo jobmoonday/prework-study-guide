3cf71ea6b98600660dcf74d629c4df443f47b300
EYUP <jobmoonday2@gmail.com>
2023-10-14 21:28:32 +0100

    for loop has been added to script.js file

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+/* if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+} */
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
